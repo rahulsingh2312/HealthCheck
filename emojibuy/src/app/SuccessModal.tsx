df3104aa8b52ed06aaeebfa50d999ef58bd62ef8
rahulsingh2312 <rahulsinghhh2312@gmail.com>
2024-11-27 00:30:59 +0530

    done

--- emojibuy/src/app/SuccessModal.tsx
+++ emojibuy/src/app/SuccessModal.tsx
@@ -6,8 +6,8 @@ const SuccessModal: React.FC<{
     if (!isOpen) return null;
   // console.log(tokens)
     return (
-      <div className="fixed inset-0 z-50 flex items-center justify-center  bg-black bg-opacity-50">
-        <div className="bg-black p-8 rounded-xl shadow-2xl  text-center max-w-md w-full relative">
+      <div className="fixed inset-0 z-50 flex  items-center justify-center  bg-black bg-opacity-50">
+        <div className="bg-black p-8 min-h-[60%] rounded-xl shadow-2xl  text-center md:min-w-[50%] min-w-[80%] relative">
           <button 
             onClick={()=>onClose} 
             className="absolute top-4 right-4 text-gray-100 hover:text-gray-800"
@@ -16,7 +16,7 @@ const SuccessModal: React.FC<{
           </button>
           <div className="flex justify-center mb-4 mt-10">
             <Rocket className="text-green-500 w-16 h-16 animate-bounce" />
-            <PartyPopper className="text-purple-500 w-16 h-16 animate-pulse" />
+            {/* <PartyPopper className="text-purple-500 w-16 h-16 animate-pulse" /> */}
           </div>
           <h2 className="text-2xl font-bold mb-4 text-green-100">Emoji Empire Expansion!</h2>
           <p className="mb-6 text-gray-300">
@@ -28,12 +28,14 @@ const SuccessModal: React.FC<{
               <span key={emoji.id} className="text-4xl">{emoji}</span>
             ))}
           </div> */}
-          <p className="text-sm text-gray-500">
-            You're not just buying Emojis, you're building a legendary emoji portfolio! 💎
+          <p className="text-sm md:mt-14  
+          text-gray-500">
+            You're not just buying Emojis,
+            <br/> you're building a legendary emoji portfolio! 💎
           </p>
           <button 
             onClick={onClose} 
-            className="mt-6 px-6 py-3 bg-gray-900 text-green-100 rounded-md hover:bg-green-100 hover:text-black transition"
+            className="md:mt-6 px-6 py-3 mt-10 bg-gray-900 text-green-100 rounded-md hover:bg-green-100 hover:text-black transition"
           >
             Buy More / Research More
           </button>
